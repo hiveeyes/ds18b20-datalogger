4252a100ad25863fe428021a49d02ba999e0e5af
Andreas Motl <andreas@hiveeyes.org>
2024-04-14 23:58:22 +0200

    Refactor code into Python package `ds18b20_datalogger`

---
+++ ds18b20_datalogger/cli.py
@@ -0,0 +1,7 @@
+from ds18b20_datalogger.core import read_ds18b20_sensor_matrix, send_measurement_mqtt
+
+
+def main():
+    reading = read_ds18b20_sensor_matrix()
+    send_measurement_mqtt(reading)
+    #  print (strftime("%Y-%m-%d %H:%M:%S", time.localtime())," Done sending. Going to sleep for 15min.")   # DEBUG
