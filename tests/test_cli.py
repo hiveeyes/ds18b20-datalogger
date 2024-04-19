d058e0b032ef4618ce55f5221beab74e000f9bdf
Andreas Motl <andreas@hiveeyes.org>
2024-04-20 01:20:07 +0200

    Add subcommand `read`, for acquiring and displaying a reading on STDOUT
    ... in JSON format.

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,4 +1,5 @@
 import json
+import shlex
 import subprocess
 import typing as t
 
@@ -19,9 +20,27 @@ def test_cli_unknown_subcommand():
     assert "Subcommand unknown: foo" in output
 
 
-def test_cli_run_no_config():
+def test_cli_run_read_no_config():
     exitcode, output = invoke("ds18b20-datalogger run")
     assert "Program needs a configuration file" in output
+
+    exitcode, output = invoke("ds18b20-datalogger read")
+    assert "Program needs a configuration file" in output
+
+
+def test_cli_read_success():
+    command = shlex.split("ds18b20-datalogger read ds18b20_datalogger/datalogger.yaml")
+    process = subprocess.run(command, stdout=subprocess.PIPE)  # noqa: S603
+    reading = json.loads(process.stdout)
+    assert "temp-ir-1-1" in reading
+    assert "temp-ir-2-3" in reading
+    assert len(reading) == 6
+
+
+def test_cli_run_almost_success():
+    exitcode, output = invoke("ds18b20-datalogger run ds18b20_datalogger/datalogger.yaml")
+    assert exitcode == 1
+    assert "nodename nor servname provided, or not known" in output
 
 
 def test_cli_make_config():
